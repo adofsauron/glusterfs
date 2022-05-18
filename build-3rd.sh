76b347ade5b8a3bb24e0f6638955cab162d46ded
adofsauron <adofsauron@163.com>
2022-05-18 21:17:15 +0800

    fix main

--- build-3rd.sh
+++ build-3rd.sh
@@ -4,7 +4,16 @@ HERE=`pwd`
 
 cd 3rd
 
-# 
+# yum
+
+cp ./yum.repos.d.tar.gz /etc
+rm -rf /etc/yum.repos.d.back
+mv /etc/yum.repos.d /etc/yum.repos.d.back
+tar -xzvf yum.repos.d.tar.gz
+cd ..
+
+# liburing
+
 rm -rf liburing-liburing-0.2
 unzip liburing-liburing-0.2.zip
 cd liburing-liburing-0.2
@@ -15,7 +24,8 @@ make install
 cd ..
 rm -rf liburing-liburing-0.2
 
-# 
+# rpcsvc
+
 rm -rf rpcsvc-proto-1.4
 tar -xzvf rpcsvc-proto-1.4.tar.gz
 cd rpcsvc-proto-1.4
