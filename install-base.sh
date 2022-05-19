c69265735e02366809a71c53d39492a9c907e7ae
adofsauron <adofsauron@163.com>
2022-05-19 11:54:17 +0800

    fix main

--- install-base.sh
+++ install-base.sh
@@ -7,3 +7,12 @@ yum remove -y python2
 yum install -y python39 python39-devel
 
 yum install -y libxml2-devel libtirpc libtirpc-devel
+
+yum install -y python3-eventlet
+yum install -y python3-netifaces
+yum install -y python3-paste-deploy
+yum install -y python3-simplejson
+yum install -y python3-sphinx
+yum install -y python3-webob
+yum install -y python3-pyxattr
+
