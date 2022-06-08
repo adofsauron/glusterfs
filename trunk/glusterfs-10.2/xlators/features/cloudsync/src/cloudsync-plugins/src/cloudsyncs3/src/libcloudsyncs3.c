5e1ec267a221b2b04447cdcc641fe326c6c5092a
adofsauron <adofsauron@163.com>
2022-06-08 15:18:15 +0800

    fix main

--- trunk/glusterfs-10.2/xlators/features/cloudsync/src/cloudsync-plugins/src/cloudsyncs3/src/libcloudsyncs3.c
+++ trunk/glusterfs-10.2/xlators/features/cloudsync/src/cloudsync-plugins/src/cloudsyncs3/src/libcloudsyncs3.c
@@ -281,7 +281,7 @@ aws_b64_encode(const unsigned char *input, int length)
     BUF_MEM *bptr;
     char *buff = NULL;
 
-    b64 = BIO_new(BIO_f_base64());
+    //b64 = BIO_new(BIO_f_base64());
     bio = BIO_new(BIO_s_mem());
     b64 = BIO_push(b64, bio);
     BIO_write(b64, input, length);
